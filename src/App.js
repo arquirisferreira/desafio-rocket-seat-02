e64ab0bd4134cb88622c5ae6b65a9c12d1e3b7f5
Arquiris <arquiris@myia.com.br>
2020-09-07 17:03:56 -0300

    Desafio completado

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+        setRepositories(response.data);
+    });
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories',{
+            
+      title: `Novo Repositório ${Date.now()}`,
+      owner: "Arquiris Ferreira"
+  
+});
+
+    const repositorie = response.data;
+
+    setRepositories([...repositories,repositorie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(
+      repository => repository.id != id
+    ))
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+            <ul>
+                {repositories.map(repos => <li key={repos.id}>{repos.title}
+                  <button onClick={() => handleRemoveRepository(repos.id)}>
+                    Remover
+                  </button>
+                </li>)}
+            </ul>
+      
+     
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+import { render } from 'react-dom';
+
+import App from './App';
+
+
+//render(<h1>Hello World</h1>, document.getElementById('app'));
+render(<App/>, document.getElementById('root'));
